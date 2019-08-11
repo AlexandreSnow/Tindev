e268f3517b3b17652ad137e36b01010c6b98a0dd
Alexandre <alexandre.souza64@fatec.sp.gov.br>
2019-08-11 14:52:50 -0300

    Finalização do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
-
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 const routes = express.Router();    
 
-routes.get('/', (request, response) => {
-    return response.json({ mensagen: `Hello ${request.query.name}` });
-});
-
-routes.post('/devs', (request, response) => {
-    console.log(request.body);
-    return response.json({ ok: true });
-})
+routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
